7c987ea49f40898827dc2ee2362e4c88c947def4
Phanh <ho.x.hung.16@gmail.com>
2022-01-16 23:29:46 +0700

    Update login

--- src/JS/login.js
+++ src/JS/login.js
@@ -1,6 +1,7 @@
 // Rest API
 
 var loginAPI='http://localhost:3000/User';
+var sessionUser='http://localhost:3000/session';
 
 // Declaration
 var username = document.querySelector("#username");
@@ -8,31 +9,54 @@ var password = document.querySelector("#password");
 
 
 
+
 const checklogin= async () =>{
-    getData(function(data){
+    getDataUser(function(data){
         const arr = data;
         for(let i=0;i<arr.length;i++){
             if(arr[i].username == username.value && arr[i].password == password.value)
-            {          
+            {         
+                var option ={
+                    id:0,
+                }; 
                 if(username.value=='admin' && password.value=='admin'){
                     window.location.href="../Dasboard/dashboard.html";
+                    option.id=arr[i].id;
+                    setDataSession(option);
                 }
                 else{
                     window.location.href="../Main/index.html";
+                    option.id=arr[i].id;
+                    setDataSession(option);
                 }
             }
         }
     })
 }
+
 function login(){
     checklogin();
 }
 //funtion
 
-function getData(callback){
+function getDataUser(callback){
     fetch(loginAPI)
         .then(function(res){
             return res.json();
         })
         .then(callback)
+}
+function setDataSession(session,callback){
+    var optionUser={
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(session)
+    }
+    fetch(sessionUser,optionUser)
+      .then(function(res){
+          res.json();
+      })
+      .then(callback)
 }
